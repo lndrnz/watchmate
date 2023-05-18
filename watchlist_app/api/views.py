e73fb5a7d19257f81dd7b62182342810e55676d3
Lander Nunez <landernunez@gmail.com>
2023-05-18 13:53:16 -0700

    added status codes

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -22,7 +22,11 @@ def movie_list(request):
 @api_view(['GET','PUT','DELETE'])
 def movie_details(request, pk):
     if request.method == "GET":
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({"Error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+    
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
@@ -33,7 +37,7 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == "DELETE":
         movie = Movie.objects.get(pk=pk)
