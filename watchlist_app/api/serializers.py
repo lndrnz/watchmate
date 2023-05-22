297fe090241913fbea036d56ac54997d9c9e5632
Lander Nunez <landernunez@gmail.com>
2023-05-22 15:13:53 -0700

    Serializer changes

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def name_length(self, value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short!")
+    else:
+        return value
+        
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validator=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -16,4 +23,19 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError("Title and description should not be the same!")
+        else:
+            return data
+        
+    
+    def validate_name(self, value):
+        
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short!")
+        else:
+            return value
         
