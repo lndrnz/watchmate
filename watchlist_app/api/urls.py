78e4c1542da876c39c5db763d04015d84a6038e7
Lander Nunez <landernunez@gmail.com>
2023-05-24 14:35:40 -0700

    changed url

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import WatchListAV, WatchDetailAV, StreamingPlatformAV, StreamDetailAV
 urlpatterns = [
     path('list/',WatchListAV.as_view(), name='watchlist'), 
-    path('<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
+    path('list/<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
     path('stream/', StreamingPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>/', StreamDetailAV.as_view(), name='stream_detail'),
 ]
